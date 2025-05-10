8ae64c9168b8a9654857b711371f1c71f1255c15
Eder-Esquivel-LP <ederesquivel142@gmail.com>
2025-05-09 21:47:08 -0600

    Se agregan los servicios de SeriesEntrenamiento

---
+++ src/main/java/EderEsquivel/bison_system/services/SeriesEntrenamientoServices.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package EderEsquivel.bison_system.services;
+
+import EderEsquivel.bison_system.model.SeriesEntrenamiento;
+import EderEsquivel.bison_system.repository.SeriesEntrenamientoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ *
+ * @author edere
+ * 
+ */
+
+@Service
+public class SeriesEntrenamientoServices {
+    @Autowired
+    private SeriesEntrenamientoRepository seR;
+
+    public SeriesEntrenamientoServices(SeriesEntrenamientoRepository seR) {
+        this.seR = seR;
+    }
+    
+    public boolean nuevaSerie(SeriesEntrenamiento se){
+        try{
+            seR.save(se); 
+            return true;  
+        }catch(Exception e){
+            return false;
+        }
+    }
+    
+    
+}
