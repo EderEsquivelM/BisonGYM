6122c4f1ec8d6bc48e675ff142a5490a86420c4f
Eder-Esquivel-LP <ederesquivel142@gmail.com>
2025-05-17 00:05:35 -0600

    Se agrega una funcion en servicios para ponder consultar datos para una grafica

--- src/main/java/EderEsquivel/bison_system/services/EntrenamientosServices.java
+++ src/main/java/EderEsquivel/bison_system/services/EntrenamientosServices.java
@@ -4,8 +4,14 @@
  */
 package EderEsquivel.bison_system.services;
 
+import EderEsquivel.bison_system.entidadesGraficas.EntrenamientoPorMes;
 import EderEsquivel.bison_system.model.Entrenamientos;
 import EderEsquivel.bison_system.repository.EntrenamientosRepository;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -26,4 +32,23 @@ public class EntrenamientosServices {
         return eR.save(entr);
     }
     
+    @PersistenceContext
+    private EntityManager entityManager;
+    
+    public List<EntrenamientoPorMes> EntrenamientosPorMes(Long idUsuario, LocalDate fechaInicio, LocalDate fechaFin) {
+        List<Object[]> results = entityManager.createNativeQuery("SELECT * FROM sp_entrenamientos_fechas(:id, :fi, :ff)")
+                .setParameter("id", idUsuario)
+                .setParameter("fi", fechaInicio)
+                .setParameter("ff", fechaFin)
+                .getResultList();
+
+        List<EntrenamientoPorMes> lista = new ArrayList<>();
+        for (Object[] row : results) {
+            String mes = (String) row[0];
+            Number totalNumber = (Number) row[1]; 
+            long total = totalNumber.longValue();
+            lista.add(new EntrenamientoPorMes(mes, total));
+        }
+        return lista;
+    }
 }
