e2208725e0fa2ff6ec40390b641d7ef72c8df178
Eder-Esquivel-LP <ederesquivel142@gmail.com>
2025-05-17 13:50:46 -0600

    Se agregan funciones para pedir datos a la base de datos

--- src/main/java/EderEsquivel/bison_system/services/GraficasServices.java
+++ src/main/java/EderEsquivel/bison_system/services/GraficasServices.java
@@ -4,7 +4,8 @@
  */
 package EderEsquivel.bison_system.services;
 
-import EderEsquivel.bison_system.entidadesGraficas.EntrenamientoPorMes;
+import EderEsquivel.bison_system.entidadesGraficas.DatosGraficaSL;
+import EderEsquivel.bison_system.entidadesGraficas.DatosGraficaSD;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import java.time.LocalDate;
@@ -21,19 +22,75 @@ public class GraficasServices {
     @PersistenceContext
     private EntityManager entityManager;
     
-    public List<EntrenamientoPorMes> EntrenamientosPorMes(Long idUsuario, LocalDate fechaInicio, LocalDate fechaFin) {
+    public List<DatosGraficaSL> EntrenamientosPorMes(Long idUsuario, LocalDate fechaInicio, LocalDate fechaFin) {
         List<Object[]> results = entityManager.createNativeQuery("SELECT * FROM sp_entrenamientos_fechas(:id, :fi, :ff)")
                 .setParameter("id", idUsuario)
                 .setParameter("fi", fechaInicio)
                 .setParameter("ff", fechaFin)
                 .getResultList();
 
-        List<EntrenamientoPorMes> lista = new ArrayList<>();
+        List<DatosGraficaSL> lista = new ArrayList<>();
         for (Object[] row : results) {
             String mes = (String) row[0];
             Number totalNumber = (Number) row[1]; 
             long total = totalNumber.longValue();
-            lista.add(new EntrenamientoPorMes(mes, total));
+            lista.add(new DatosGraficaSL(mes, total));
+        }
+        return lista;
+    }
+    
+    public List<DatosGraficaSD> evolucionPesoCargado(Long idUsuario, Integer idEjercicio, LocalDate fechaInicio, LocalDate fechaFin) {
+        List<Object[]> results = entityManager.createNativeQuery(
+                "SELECT * FROM sp_evolucion_peso_cargado(:idUsuario, :idEjercicio, :fechaInicio, :fechaFin)")
+                .setParameter("idUsuario", idUsuario)
+                .setParameter("idEjercicio", idEjercicio)
+                .setParameter("fechaInicio", fechaInicio)
+                .setParameter("fechaFin", fechaFin)
+                .getResultList();
+
+        List<DatosGraficaSD> lista = new ArrayList<>();
+        for (Object[] row : results) {
+            // Cast directo a Date y luego conversión a LocalDate
+            String fecha = (String) row[0];
+            Number pesoNumber = (Number) row[1];
+            double pesoUsado = pesoNumber.doubleValue();
+            lista.add(new DatosGraficaSD(fecha, pesoUsado));
+        }
+        return lista;
+    }
+    
+    public List<DatosGraficaSD> evolucionPesoUsuario(Long idUsuario, LocalDate fechaInicio, LocalDate fechaFin){
+        List<Object[]> results = entityManager.createNativeQuery(
+                "SELECT * FROM sp_evolucion_peso(:idUsuario, :fechaInicio, :fechaFin)")
+                .setParameter("idUsuario", idUsuario)
+                .setParameter("fechaInicio", fechaInicio)
+                .setParameter("fechaFin", fechaFin)
+                .getResultList();
+
+        List<DatosGraficaSD> lista = new ArrayList<>();
+        for (Object[] row : results) {
+            String fecha = (String) row[0];
+            Number pesoNumber = (Number) row[1];
+            double pesoUsado = pesoNumber.doubleValue();
+            lista.add(new DatosGraficaSD(fecha, pesoUsado));
+        }
+        return lista;
+    }
+    
+    public List<DatosGraficaSL> evolucionPGC(Long idUsuario, LocalDate fechaInicio, LocalDate fechaFin){
+        List<Object[]> results = entityManager.createNativeQuery(
+                "SELECT * FROM sp_evolucion_grasa(:idUsuario, :fechaInicio, :fechaFin)")
+                .setParameter("idUsuario", idUsuario)
+                .setParameter("fechaInicio", fechaInicio)
+                .setParameter("fechaFin", fechaFin)
+                .getResultList();
+
+        List<DatosGraficaSL> lista = new ArrayList<>();
+        for (Object[] row : results) {
+            String fecha = (String) row[0];
+            Number pesoNumber = (Number) row[1];
+            long pesoUsado = pesoNumber.longValue();
+            lista.add(new DatosGraficaSL(fecha, pesoUsado));
         }
         return lista;
     }
