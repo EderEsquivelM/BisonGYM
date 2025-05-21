c0b1312c83a9d8ff6cbf8eea6df168ea57bbc095
Eder-Esquivel-LP <ederesquivel142@gmail.com>
2025-05-20 18:35:50 -0600

    Se agrega documentacion

--- src/main/java/EderEsquivel/bison_system/entidadesGraficas/DatosGraficaSD.java
+++ src/main/java/EderEsquivel/bison_system/entidadesGraficas/DatosGraficaSD.java
@@ -5,35 +5,48 @@
 package EderEsquivel.bison_system.entidadesGraficas;
 
 /**
- *
+ * Clase que guarda los datos de un tipo de grafico con datos Double.
+ * 
  * @author edere
  */
 public class DatosGraficaSD {
+    
+    /**
+     * Guarda el campo se esta graficando(ejemplo: kg,nombre del mes).
+     */
     private String campo;
+    
+    /**
+     * Guarda el valor del campo graficado.
+     */
     private double dato;
 
+    /**
+     * Constructor que guarda el campo y su valor que se graficara.
+     * 
+     * @param campo Campo se esta graficando(ejemplo: kg,nombre del mes).
+     * @param dato Valor del campo graficado.
+     */
     public DatosGraficaSD(String campo, double dato) {
         this.campo = campo;
         this.dato = dato;
     }
 
+    /**
+     *
+     * @return el campo que se esta graficando.
+     */
     public String getCampo() {
         return campo;
     }
 
-    public void setCampo(String campo) {
-        this.campo = campo;
-    }
-
+    
+    /**
+     *
+     * @return el valor de campo graficado.
+     */
     public double getDato() {
         return dato;
     }
 
-    public void setDato(double dato) {
-        this.dato = dato;
-    }
-
-    
-    
-    
 }
--- src/main/java/EderEsquivel/bison_system/entidadesGraficas/DatosGraficaSL.java
+++ src/main/java/EderEsquivel/bison_system/entidadesGraficas/DatosGraficaSL.java
@@ -5,35 +5,47 @@
 package EderEsquivel.bison_system.entidadesGraficas;
 
 /**
- *
+ *Clase que guarda los datos de un tipo de grafico con datos Long.
+ * 
  * @author edere
  */
 public class DatosGraficaSL {
+    
+     /**
+     * Guarda el campo se esta graficando(ejemplo: kg,nombre del mes).
+     */
     private String campo;
+    
+     /**
+     * Guarda el valor del campo graficado.
+     */
     private Long dato;
 
+    /**
+     * Constructor que guarda el campo y su valor que se graficara.
+     * 
+     * @param campo Campo se esta graficando(ejemplo: kg,nombre del mes).
+     * @param dato Valor del campo graficado.
+     */
     public DatosGraficaSL(String campo, Long dato) {
         this.campo = campo;
         this.dato = dato;
     }
 
+    /**
+     *
+     * @return el campo que se esta graficando.
+     */
     public String getCampo() {
         return campo;
     }
 
-    public void setCampo(String campo) {
-        this.campo = campo;
-    }
-
+    /**
+     *
+     * @return el valor de campo graficado.
+     */
     public Long getDato() {
         return dato;
     }
-
-    public void setDato(Long dato) {
-        this.dato = dato;
-    }
-
-    
-    
     
 }
